7f514ee2fef0f3d535d4baf0a666948342231204
Miranda Cooper <mleighc@umich.edu>
2023-03-27 18:21:02 -0400

    starting hw6

--- js/video.js
+++ js/video.js
@@ -1,11 +1,27 @@
-var video;
+var video = document.querySelector('#player1');
 
-window.addEventListener("load", function() {
-	console.log("Good job opening the window")
-
+window.addEventListener("load", function () {
+	video.autoplay = false;
+	video.loop = false;
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function () {
+	video.play();
+});
+
+document.querySelector("#pause").addEventListener("click", function () {
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function () {
+	video.playbackRate = 0.5;
+});
+
+document.querySelector("#faster").addEventListener("click", function () {
+	video.playbackRate = 1.5;
+});
+
+document.querySelector("#skip").addEventListener("click", function () {
+	video.currentTime += 10.0
+});
 
